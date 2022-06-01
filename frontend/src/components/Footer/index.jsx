6176d4cb8af1a30acce9ae9c381e6e40655f6822
Tran Nhut <tnhut803@gmail.com>
2022-06-01 14:22:42 +0700

    added shopee link

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -43,21 +43,21 @@ const Footer = () => {
     }
   };
   return (
-    <section className='footer'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-6 col-xs-12'>
-            <h1 className='logo'>
+    <section className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 col-xs-12">
+            <h1 className="logo">
               <img
-                src='https://cf.shopee.vn/file/0ce0d46a858fb639ff4629cb881207cc_tn'
-                alt=''
+                src="https://cf.shopee.vn/file/0ce0d46a858fb639ff4629cb881207cc_tn"
+                alt=""
               />
               VFruit
             </h1>
             <br />
             <p>
               Doanh nghiệp chúng tôi với phương châm{" "}
-              <span className='secon-color'>khách hàng là trọng tâm,</span> là
+              <span className="secon-color">khách hàng là trọng tâm,</span> là
               định hướng để phát triển doanh nghiệp. Với định hướng này trong
               những năm tới chúng tôi sẻ tiếp tục cải thiện cơ sở vật chất, sản
               phẩm, đổi ngũ nhân lực để mang thương hiệu của mình tới nhiều
@@ -68,40 +68,49 @@ const Footer = () => {
             <br />
             <p>
               Email:{" "}
-              <a href='mailto: vfruit-uit@outlook.com' className='secon-color'>
+              <a href="mailto: vfruit-uit@outlook.com" className="secon-color">
                 vfruit-uit@outlook.com
               </a>
             </p>
             <p>
               Phone:{" "}
-              <a href='tel: 0983406631' className='secon-color'>
+              <a href="tel: 0983406631" className="secon-color">
                 098 340 6631
               </a>
             </p>
             <p>
               Địa chỉ:{" "}
-              <span className='secon-color'>
+              <span className="secon-color">
                 4 Lê Duẩn, Bến Nghế, Quận 1, Tp Hồ Chí Minh
               </span>
             </p>
+            <p>
+              Gian hàng Shopee:{" "}
+              <a
+                href="https://shopee.vn/vfruit.official"
+                className="secon-color"
+              >
+                VFruit Offical
+              </a>
+            </p>
           </div>
-          <div className='col-2 col-xs-12'>
+          <div className="col-2 col-xs-12">
             <h1>Về VFruit</h1>
             <br />
             <p>
-              <Link to='#'>Nhà sáng lập</Link>
+              <Link to="#">Nhà sáng lập</Link>
             </p>
             <p>
-              <Link to='#product-menu'>Sản phẩm của chúng tôi</Link>
+              <Link to="#product-menu">Sản phẩm của chúng tôi</Link>
             </p>
             <p>
-              <Link to='#testimonial'>Đánh giá người dùng</Link>
+              <Link to="#testimonial">Đánh giá người dùng</Link>
             </p>
             <p>
-              <Link to='#'>Bài viết</Link>
+              <Link to="#">Bài viết</Link>
             </p>
           </div>
-          <div className='col-4 col-xs-12'>
+          <div className="col-4 col-xs-12">
             <h1>Đăng ký nhận thông tin</h1>
             <br />
             <p>
@@ -110,12 +119,12 @@ const Footer = () => {
               {"    "}Chúng tôi sẽ gửi bạn email mỗi khi có những deals hấp dẫn
               cũng như thông báo về những sản phẩm mới mà bạn có thể hứng thú.
             </p>
-            <div className='input-group'>
+            <div className="input-group">
               <input
-                type='text'
+                type="text"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder='Nhập email của bạn'
+                placeholder="Nhập email của bạn"
               />
               <button onClick={handleRegister}>Đăng ký</button>
             </div>
